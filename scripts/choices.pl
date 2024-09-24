7cbd46ab37de9d473c7104789e98bf4e7f7a461d
Max Maischein <corion@corion.net>
2024-09-24 13:44:35 +0200

    Add to_JSON()
    ... but this only converts the JSON data, not the whole object to JSON

--- scripts/choices.pl
+++ scripts/choices.pl
@@ -5,7 +5,7 @@ package Choice::Choice;
 use 5.020;
 use experimental 'signatures';
 use Moo 2;
-use Mojo::JSON 'decode_json';
+use Mojo::JSON 'decode_json', 'encode_json';
 
 has 'choice_id' => (
     is => 'ro',
@@ -26,6 +26,10 @@ sub from_row( $class, $row ) {
     $row->{choice_json} = decode_json($row->{choice_json})
         if defined $row->{choice_json};
     return $class->new({ $row->%* });
+}
+
+sub to_JSON( $self ) {
+    return encode_json( $self->choice_json );
 }
 
 package Choice::Question 0.01;
